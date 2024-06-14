3e67ba53cb4bb53c3e9195a7c5dcf2cbb234ffdc
Mikey191 <desertigorb@gmail.com>
2024-06-14 11:46:26 +0300

    create User Controller

---
+++ controller/user.controller.js
@@ -0,0 +1,15 @@
+const db = require("../db");
+
+class UserController {
+  async createGroup(ctx) {}
+  async createExercises(ctx) {}
+  async createUser(ctx) {}
+  async getGroups(ctx) {}
+  async getExercises(ctx) {}
+  async getUsers(ctx) {}
+  async deleteGroup(ctx) {}
+  async deleteExercise(ctx) {}
+  async deleteUser() {}
+}
+
+module.exports = new UserController();
